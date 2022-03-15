5ed4dd1e4406228bc34da19a0a3bd238f9e48ade
Nick Rader <69931749+nrader2000@users.noreply.github.com>
2022-03-15 17:04:30 -0400

    added project folder and description

---
+++ ATC-Sim-Project/index.py
@@ -0,0 +1,7 @@
+# Joe Davidson, Sarah McLellan, Nick Rader, Declan Worley
+# This project is the capstone project for our CSC-4710 Software Engineering Class
+# The product we were assigned with developing is an Air Traffic Control Simulator, based on the atc-sim.com web app. This 
+# should be able to simulate planes approaching and taking off from the STL (St. Louis Lambert) airport, as well as planes passing 
+# through the area. The product should also give the user the ability to act as an air traffic controller by giving the user
+# the necessary commands to be able to direct and control each of the simulated planes around the airport. Just as any Air Traffic 
+# Controller would be able to.  
